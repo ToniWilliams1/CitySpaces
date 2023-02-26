387fcb72723eadebafb7a177a3e2354d8208cc19
Toni Williams <williata04@mail.buffalostate.edu>
2023-02-26 04:06:24 -0500

    add filter functionality

--- controllers/events.js
+++ controllers/events.js
@@ -7,10 +7,49 @@ const User = require("../models/User");
     getEvents: async (req, res) => {
         const results = await fetch('https://data.cityofnewyork.us/resource/bquu-z2ht.json');
        const data = await results.json();
-        console.log(data[0].title);
+      //  const { selectedRecurrenceType } = req.query;
 
-        res.render("../views/events.ejs", {arrayOfData: data})
-      },
+      // //  let filteredData;
+
+      // //  if (borough) {
+      // //    filteredData = data.filter(event=> event.borough === borough);
+      // //  } else {
+      // //    filteredData = data;
+      // //  }
+      // const selectedRecurrenceType = req.query.recurrenceType;
+      
+      // // Filter the events data by selected borough (or show all events if no borough is selected)
+      // const filteredData = selectedRecurrenceType ? data.filter(event => event.recurrence_type === selectedRecurrenceType) : data;
+      // console.log(selectedRecurrenceType)
+      //           res.render('events', { events: filteredData, selectedRecurrenceType });
+      
+// If selectedRecurrenceType is provided, filter the data by recurrence type
+// if (selectedRecurrenceType) {
+
+//   data = data.filter(event => event.recurrence_type === selectedRecurrenceType);
+//         console.log(selectedRecurrenceType)
+const { recurrence_type = [], borough = [] } = req.query;
+// const filteredData = recurrence_type ? data.filter(event => event.recurrence_type === recurrence_type) : data;
+const filteredData = data.filter(event => (
+  recurrence_type.length === 0 || recurrence_type.includes(event.recurrence_type)
+) && (
+  borough.length === 0 || borough.includes(event.borough)
+));
+res.render('../views/events.ejs', { events: filteredData });
+
+},
+
+
+
+
+// res.render('events', { events: data });
+
+
+
+        // const newArray = data.filter(item=> item.recurrence_type === "ongoing")
+        // res.render("../views/events.ejs", {arrayOfData: filteredData})
+      
+      
 
       likeEvent: async (req, res) => {
         try {
